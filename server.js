bb018fd5becf1fb7dd889b25c7be66c235cc7f7d
Alan <b.alan.carrion@gmail.com>
2025-02-08 01:41:49 -0600

    Creacion de archivos

---
+++ server.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const  app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+const fs = require('fs').promises;
+
+async function obtenerTareas() {
+  const data = await fs.readFile('tareas.json', 'utf8');
+  return JSON.parse(data);
+}
+
+async function guardarTareas(tareas) {
+  await fs.writeFile('tareas.json', JSON.stringify(tareas));
+}
+
+
+app.use(express.json());
+
+//Funciones
+//funcion para leer JSON y obtener datos
+
+
+async function obtenerTareas() {
+const data = await fs.readFile('tareas.json', 'utf8');
+return JSON.parse(data);
+}
+
+async function guardarTareas(tareas) {
+await fs.writeFile('tareas.json', JSON.stringify(tareas));
+}
+
+
+app.get('/',(req,res) => {
+
+res.send('Bienvenido a la app');
+
+var tareas = obtenerTareas();
+res.send(`Las tareas actuales son ${tareas} `)
+
+});
+
+app.post('/tareas', async(req,res) => {
+//const tareaId = parseInt(req.params.id);
+
+
+let nuevaTarea = req.body
+
+const {titulo, descripcion} = req.body;
+
+var tareas = await obtenerTareas();
+
+const indice = Object.keys(tareas)
+var newindice = indice.length > 0 ? Math.max(...indice.map(Number))+1:1;
+const tarea = {id: newindice,titulo,descripcion};
+
+tareas.push(tarea);
+
+await guardarTareas(tareas);
+
+res.status(201).send(`La tarea ${titulo} fue creada exitosamente`)
+});
+
+
+app.put('/:id', async(req,res) => {
+const tareaId = parseInt(req.params.id);
+var id = [0]
+const datosNuevos = req.body;
+
+const tareas = await obtenerTareas();
+
+const tareaObjetivo = tareas.findIndex((tareita) => tareita.id === tareaId);
+if (!tareaObjetivo){
+res.status(401);
+}
+
+tareas[tareaObjetivo] = {...tareas[tareasObjetivo], ...datosNuevos};
+
+await guardarTareas(tareas);
+
+res.json(tareas[tareaObjetivo]);
+
+});
+
+
+
+app.delete('/tareas',(req,res) => {
+
+});
+
+
+
+
+
+app.listen(PORT, () => {
+console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+
+app.use("/", (req, res, next) =>{
+console.log(`Metodo ${req.method} recibido`)
+next()
+})
